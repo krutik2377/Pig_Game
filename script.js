4eaea662e101ee6b87e1ffa7bba3f076822af4ac
krutik2377 <krutikgevariya7723@gmail.com>
2023-08-24 16:27:11 -0400

    Structurise the basic things

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+// # is for selecting id instead of class which is (.)
+// Selecting elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+
+// hidden dice before game starts.
+diceEL.classList.add('hidden');
+
