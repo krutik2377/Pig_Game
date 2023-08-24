e0cb2b21c84914368c156531c4d8a525f54b7771
krutik2377 <krutikgevariya7723@gmail.com>
2023-08-24 17:38:02 -0400

    switching the player Feature added.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 // # is for selecting id instead of class which is (.)
 // Selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const current0EL = document.getElementById('current--0');
@@ -13,9 +15,11 @@ const btnHold = document.querySelector('.btn--hold');
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 
+const scores = [0,0];
 // hidden dice before game starts.
 diceEL.classList.add('hidden');
 let currentScore =0;
+let activePlayer =0;
 
 btnRoll.addEventListener('click', function() {
     //1. Generating random dice.
@@ -30,10 +34,15 @@ btnRoll.addEventListener('click', function() {
     {
         // Add dice to the current score.
         currentScore += dice;
-        current0EL.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }
     else{
-        
+        // Switch to next player.
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore =0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0EL.classList.toggle('player--active');
+        player1EL.classList.toggle('player--active');
     }
     
 });
